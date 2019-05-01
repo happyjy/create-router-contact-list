52cc2eb32b93d898e299cded4f1565581f327bfb
happyjy <okwoyjy@gmail.com>
2019-05-01 18:26:13 +0900

    #3-9 Contact List App- Created Home page + Contacts

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from "react";
-
+import { Route } from "react-router-dom";
 class App extends Component {
   render() {
     return (
@@ -15,10 +15,30 @@ class Header extends Component {
     return (
       <header>
         <h1>My Contacts</h1>
+        <Route exact path="/" component={Welcome} />
+        <Route path="/contacts" component={contacts} />
       </header>
     );
   }
 }
+
+// class Welcome extends Component {
+//   render() {
+//     return (
+//       <h1>Wlcome to the best contact app!</h1>
+//     );
+//   }
+// }
+
+const Welcome = () => <h1>Wlcome to the best contact app!</h1>;
+
+const contacts = () => (
+  <ul>
+    <li>Jyoon</li>
+    <li>SoEun</li>
+    <li>HAHA</li>
+  </ul>
+)
 
 // const App = () => {
 //   <div>
